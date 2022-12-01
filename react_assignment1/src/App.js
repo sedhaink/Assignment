f7746eae65a03b05202422d685e7641ad5ff2fff
Kabindra Sedhain <k.sedhain1@gmail.com>
2022-11-30 19:07:32 -0600

    Modified app.js and index.js to start react app

--- react_assignment1/src/App.js
+++ react_assignment1/src/App.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <article class="recipe">
+      <h2>Chicken Vindaloo</h2>
+      <p>
+        “Vindaloo” is a popular Indian that calls for meat to be marinated in a
+        highly flavorful spicy mixture with vinegar, then quickly cooked up when
+        you’re ready to eat.
+      </p>
+      <p>
+        While this dish has a long list of spices, most are quite common in the
+        average spice rack. If you find you're missing one, it's not a deal
+        breaker. You will still have a lovely, fragrant curry.
+      </p>
+      <aside>
+        <p>
+          Quick warning:{" "}
+          <strong>
+            this recipe is <em>spicy</em>
+          </strong>
+          . for an alternative recipe with less heat.
+        </p>
+      </aside>
+    </article>
+  );
 }
 
 export default App;
